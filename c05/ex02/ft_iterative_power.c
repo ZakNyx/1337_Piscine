016a6dc5d7728e9418d1fa654884077543b2182c
Zakaria IHIRRI <69282671+ZakNyx@users.noreply.github.com>
2021-08-02 22:52:34 +0200

    Add files via upload

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zihirri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/03 12:12:23 by zihirri           #+#    #+#             */
+/*   Updated: 2021/07/07 15:59:31 by zihirri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 1)
+	{
+		result = result * nb;
+		power--;
+	}
+	return (result);
+}
